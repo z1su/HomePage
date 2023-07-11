4f42d1d0a056cdc4cde78c6fc5741566d133c22d
Song ji su <70136320+z1su@users.noreply.github.com>
2023-07-11 22:23:33 +0900

    chore: css library 세팅
    tailwind, dasiyui 세팅

--- oldrookie/tailwind.config.js
+++ oldrookie/tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/list/page.js",
+    "./app/layout.js",
+    "./app/list/layout.js",
+    "./compnents/header/nav.js",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    backgroundImage: {
+      title: "url('/images/title.png')",
     },
+
+    extend: {},
+    // screens: {
+    //   m: { min: "390px", max: "425px" },
+    //   t: { min: "426px", max: "900px" },
+    //   d: { min: "900px", max: "1440px" },
+    //   dl: { min: "1440px" },
+    // },
   },
-  plugins: [],
-}
+
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
+};
