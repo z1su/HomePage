dbfe3ce9718dceb946a37a4e6479abb971deceb4
Song ji su <0_0727@naver.com>
2023-07-17 21:57:45 +0900

    remove: 파일 이동 및 삭제

--- oldrookie/components/header/name.js
+++
@@ -1,9 +0,0 @@
-export default function Name({ name }) {
-  return (
-    <div>
-      <h1 className="ps-[9.375rem] pt-[0.5rem] text-white text-6xl font-semibold leading-[72px]">
-        {name}
-      </h1>
-    </div>
-  );
-}
