4850aea41e034f7ad9f68db670d15b98531650fa
Song ji su <0_0727@naver.com>
2023-07-17 21:56:16 +0900

    design: header 구현

---
+++ oldrookie/app/components/header/subname.js
@@ -0,0 +1,9 @@
+export default function SubName({ subname }) {
+  return (
+    <div>
+      <h2 className="ps-[9.375rem] text-white font-open-sans text-2xl font-normal leading-[20px]">
+        {subname}
+      </h2>
+    </div>
+  );
+}
