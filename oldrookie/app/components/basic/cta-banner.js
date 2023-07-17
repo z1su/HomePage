9e59e22e35f4b2264d80a3906dc5866a4567af0d
Song ji su <0_0727@naver.com>
2023-07-17 21:55:51 +0900

    design: cta banner 구현

---
+++ oldrookie/app/components/basic/cta-banner.js
@@ -0,0 +1,16 @@
+export default function BannerWithCTA() {
+  return (
+    <div class="bg-slate-800 flex items-center justify-center xl:min-h-[50vh] ">
+      <div class="">
+        <div class="flex flex-col p-10  px-16 space-y-6 items-center text-center">
+          <h1 class="font-light md:text-4xl text-6xl text-white tracking-wide ">
+            We will be your success partner, <br></br> not a development agency.
+          </h1>
+          <button class="rounded-full bg-stone-900 shadow-lg text-white text-lg py-4 px-6">
+            Contact Us →
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
