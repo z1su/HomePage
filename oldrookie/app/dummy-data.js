3486968609871f7994f3b01eb7330fb8215e98e8
Song ji su <0_0727@naver.com>
2023-07-19 20:37:13 +0900

    docs: dummy data 추가

--- oldrookie/app/dummy-data.js
+++ oldrookie/app/dummy-data.js
@@ -4,6 +4,8 @@ const PROJECT = [
     title: "빈센트 반 고흐전",
     subtitle: "WebVR 전시회 (전체 공간 VR 제작)",
     category: "VR/AR",
+    content:
+      "현실에서 한번에 관람하기 힘든 빈센트 반 고흐 작가의 유명작들을\n가상 공간 전시회를 통해 하나의 전시회에서 모두 관람하실 수 있습니다.\n광원에 따라 그림의 붓 터치 등 물감의 질감까지 확인할 수 있는 고화질 콘텐츠입니다.\nmobile Phone, PC, Tablet, VR HMD 등 다양한 환경에서 관람 가능합니다.",
     isFeatured: false,
   },
 
@@ -13,6 +15,8 @@ const PROJECT = [
     subtitle: "WebVR 전시회 (전체 공간 VR 제작)",
     category: "VR/AR",
     isFeatured: false,
+    content:
+      "신진작가 조혜원씨의 2019년 컬랙션 전시회입니다.\n작가와 작품의 아이덴티티에 맞는 전시 공간 기획 및 구현으로\n시간과 공간의 제약 없이 작품을 관람하실 수 있습니다.\nmobile Phone, PC, Tablet, VR HMD 등 다양한 환경에서 관람 가능합니다.",
   },
 
   {
@@ -21,13 +25,15 @@ const PROJECT = [
     subtitle: "WebVR 전시회 (360º VR 촬영)",
     category: "VR/AR",
     isFeatured: false,
+    content:
+      "2019.12.19~2019.12.31 서울 조형갤러리(chohyong gallery)에서 진행되었던\n조혜원작가의 ‘시작된 노랑씨의 여정' 전시회를 360º 촬영후 가공하여\n영구적으로 보관 및 관람이 가능한 VR 전시회로 기록하였습니다.\nmobile Phone, PC, Tablet, VR HMD 등 다양한 환경에서 관람 가능합니다.",
   },
 
   {
     id: "4",
     title: "DAUSPICE",
     subtitle: "브랜드 show window 3D modeling",
-    category: "3D Modeling",
+    category: "Space 3D Modeling",
     isFeatured: false,
   },
 
@@ -35,7 +41,7 @@ const PROJECT = [
     id: "5",
     title: "Loom Loom",
     subtitle: "브랜드 show window 3D modeling",
-    category: "3D Modeling",
+    category: "Space 3D Modeling",
     isFeatured: false,
   },
 
@@ -43,14 +49,14 @@ const PROJECT = [
     id: "6",
     title: "Sukee",
     subtitle: "브랜드 show window 3D modeling",
-    category: "3D Modeling",
+    category: "Space 3D Modeling",
     isFeatured: false,
   },
   {
     id: "7",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     isFeatured: false,
   },
 
@@ -58,7 +64,7 @@ const PROJECT = [
     id: "8",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     isFeatured: false,
   },
 
@@ -66,7 +72,7 @@ const PROJECT = [
     id: "9",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     isFeatured: false,
   },
 
@@ -74,7 +80,7 @@ const PROJECT = [
     id: "10",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     isFeatured: false,
   },
 
@@ -82,7 +88,7 @@ const PROJECT = [
     id: "11",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     isFeatured: false,
   },
 
@@ -98,7 +104,7 @@ const PROJECT = [
     id: "13",
     title: "SAY-_YEON",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     isFeatured: false,
   },
 
@@ -106,7 +112,7 @@ const PROJECT = [
     id: "14",
     title: "SAY-_YEON",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -115,7 +121,7 @@ const PROJECT = [
     id: "15",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -124,7 +130,7 @@ const PROJECT = [
     id: "16",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -133,7 +139,7 @@ const PROJECT = [
     id: "17",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -151,7 +157,7 @@ const PROJECT = [
     id: "19",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -160,7 +166,7 @@ const PROJECT = [
     id: "20",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -169,7 +175,7 @@ const PROJECT = [
     id: "21",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -178,7 +184,7 @@ const PROJECT = [
     id: "22",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -187,7 +193,7 @@ const PROJECT = [
     id: "23",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -196,7 +202,7 @@ const PROJECT = [
     id: "24",
     title: "Loom Loom",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -205,7 +211,7 @@ const PROJECT = [
     id: "25",
     title: "Sukee",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -214,7 +220,7 @@ const PROJECT = [
     id: "26",
     title: "Sukee",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -223,7 +229,7 @@ const PROJECT = [
     id: "27",
     title: "AODE",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -232,7 +238,7 @@ const PROJECT = [
     id: "28",
     title: "DAUSPICE",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -241,7 +247,7 @@ const PROJECT = [
     id: "29",
     title: "DAUSPICE",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -250,7 +256,7 @@ const PROJECT = [
     id: "30",
     title: "DAUSPICE",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -259,7 +265,7 @@ const PROJECT = [
     id: "31",
     title: "DAUSPICE",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -267,7 +273,7 @@ const PROJECT = [
     id: "32",
     title: "DAUSPICE",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -275,7 +281,7 @@ const PROJECT = [
     id: "33",
     title: "DAUSPICE",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -284,7 +290,7 @@ const PROJECT = [
     id: "34",
     title: "Assemble Station",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -293,7 +299,7 @@ const PROJECT = [
     id: "35",
     title: "Assemble Station",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -302,7 +308,7 @@ const PROJECT = [
     id: "36",
     title: "Assemble Station",
     subtitle: "패션 상품 3D Modeling",
-    category: "3D Modeling",
+    category: "Fashion 3D Modeling",
     image: "images/coding.jpg",
     isFeatured: false,
   },
@@ -403,6 +409,16 @@ export function getFilteredEvents(dateFilter) {
 
 export function getEventById(id) {
   return PROJECT.find((event) => event.id === id);
+}
+
+export function getFilteredCategory(categoryFilter) {
+  const { category } = categoryFilter;
+
+  let filteredCategory = PROJECT.filter((event) => {
+    return event.category === category;
+  });
+
+  return filteredCategory;
 }
 
 function Parent() {
