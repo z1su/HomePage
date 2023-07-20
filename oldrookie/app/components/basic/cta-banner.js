8db86713c01620d0f9b6fa9443844953ab9c3bdd
Song ji su <0_0727@naver.com>
2023-07-20 16:33:47 +0900

    design: 반응형 수정

--- oldrookie/app/components/basic/cta-banner.js
+++ oldrookie/app/components/basic/cta-banner.js
@@ -1,6 +1,6 @@
 export default function BannerWithCTA() {
   return (
-    <div class="bg-slate-800 flex items-center w-screen justify-center max-h-full  bg-footer">
+    <div class="bg-slate-800 flex items-center w-screen justify-center max-h-full bg-footer">
       <div class="">
         <div class="flex flex-col p-10  px-16 space-y-6 items-center text-center">
           <p class="font-light md:text-4xl text-6xl text-white tracking-wide">
--- oldrookie/app/layout.js
+++ oldrookie/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import Navbar from "@/app/components/header/nav";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" className="flex items-center">
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 oldrookie
@@ -109,13 +109,9 @@ export default function RootLayout({ children }) {
             </div>
           </div>
         </nav>
-
         {/* <Navbar label="Menu" items={menuItems} /> */}
-
         {/* <Navbar /> */}
-
         {children}
-
         <footer className="bg-white shadow">
           <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div className="sm:flex sm:items-center sm:justify-between">
@@ -123,7 +119,6 @@ export default function RootLayout({ children }) {
                 href="https://flowbite.com/"
                 className="flex items-center mb-4 sm:mb-0"
               ></a>
-
               <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
                 <li>
                   <a href="#" className="mr-4 hover:underline md:mr-6 ">
