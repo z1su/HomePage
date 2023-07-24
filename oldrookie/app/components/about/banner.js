bda7aa3063aba7331b0303aea01e6efd6b501ce4
Song ji su <0_0727@naver.com>
2023-07-24 21:52:08 +0900

    feat: hover시 이미지, backgroud 변경

--- oldrookie/app/components/about/banner.js
+++ oldrookie/app/components/about/banner.js
@@ -1,57 +1,60 @@
 export default function AboutBanner() {
   return (
-    <div className="w-screen mx-auto p-4 md:py-8">
-      <div className="sm:flex sm:items-center sm:px-5 justify-between">
-        <div className="mx-2">
+    <div
+      className="w-full pt-4 main_container sm:sm_container pb-6  mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16
+    max-w-7xl"
+    >
+      <div className="flex flex-row justify-between sm:flex sm:flex-col sm:items-center sm:px-5">
+        <div className="sm:mb-3">
           <a
             href="#"
-            className="flex flex-col w-50 h-80 p-6 border rounded-lg hover:bg-gray-100"
+            className="group flex flex-col max-w-custom-size h-80 p-6 border rounded-lg hover:bg-main-color"
           >
-            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white">
               01
             </h4>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white">
               상생 <br></br> Win-win
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-normal dark:text-gray-400 group-hover:text-white">
               올드루키는 조직과 이해관계자들의 상호 이익을 고려하며, 상호 협력을
               통해 모두에게 이로운 결과를 창출할 수 있도록 노력합니다.
             </p>
           </a>
         </div>
 
-        <div className="mx-2">
+        <div className="sm:mb-3">
           <a
             href="#"
-            className="flex flex-col w-50 h-80 p-6 border rounded-lg hover:bg-gray-100"
+            className="group flex flex-col max-w-custom-size h-80 p-6 border rounded-lg hover:bg-main-color"
           >
-            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white">
               02
             </h4>
 
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white">
               신뢰 <br></br> Flexibility
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-normal dark:text-gray-400 group-hover:text-white">
               올드루키는 변화하는 시장 조건과 비즈니스 환경에 빠르게 대응하고
               조정할 수 있도록 개방적이고 유연한 조직 문화를 운영합니다.
             </p>
           </a>
         </div>
 
-        <div className="mx-2">
+        <div className="sm:mb-3">
           <a
             href="#"
-            className="flex flex-col w-50 h-80 p-6 border rounded-lg hover:bg-gray-100"
+            className="group flex flex-col max-w-custom-size h-80 p-6 border rounded-lg hover:bg-main-color"
           >
-            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white">
               03
             </h4>
 
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white">
               유연성 <br></br> Flexibility
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-normal text-gray-700 dark:text-gray-400 group-hover:text-white">
               올드루키는 변화하는 시장 조건과 비즈니스 환경에 빠르게 대응하고
               조정할 수 있도록 개방적이고 유연한 조직 문화를 운영합니다.
             </p>
--- oldrookie/app/components/project/banner.js
+++ oldrookie/app/components/project/banner.js
@@ -1,55 +1,62 @@
 export default function Banner() {
   return (
-    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-      <div className="sm:flex sm:items-center sm:px-5 justify-between">
-        <div>
+    <div
+      className="w-full pt-4 main_container sm:sm_container pb-6  mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16
+    max-w-7xl"
+    >
+      <div className="flex flex-row justify-between sm:flex sm:flex-col sm:items-center sm:px-5">
+        <div className="sm:mb-3">
           <a
-            href="#"
-            className="block w-80 h-80 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+            href="/list/vrar"
+            className="group flex flex-col max-w-custom-size h-80 p-6 border rounded-lg hover:bg-banner01 hover:bg-cover"
           >
-            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-transparent">
               01
             </h4>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-transparent">
               VR/AR
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-normal text-gray-700 dark:text-gray-400 group-hover:text-transparent">
               VR(가상현실)과 AR(증강현실)을 통해 새로운 시각적 체험과 상호작용을
               할 수 있으며 교육, 엔터테인먼트, 훈련 등 다양한 분야에서 활용
               가능합니다.
             </p>
           </a>
         </div>
-        <div className="px-8">
+
+        <div className="sm:mb-3">
           <a
             href="#"
-            className="block w-80 h-80 p-6 border rounded-lg hover:bg-gray-100"
+            className="group flex flex-col max-w-custom-size h-80 p-6 border rounded-lg hover:bg-banner02 hover:bg-cover"
           >
-            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-transparent">
               02
             </h4>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-transparent">
               3D 모델링
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              웹 빌드, 촬영, 가공 및 편집으로 가상공간을 제작하여 기업의 브랜드
+            <p className="font-normal text-gray-700 dark:text-gray-400 group-hover:text-transparent">
+              웹 빌드, 촬영, 가공 및 편집으로 가상공간 제작하여 기업의 브랜드
               아이덴티티를 효과적으로 표현하고 상품 소개 상세페이지를 보다
               풍성하게 채울 수 있습니다.
             </p>
           </a>
         </div>
-        <div>
+
+        <div className="sm:mb-3">
           <a
             href="#"
-            class="block w-80 h-80 p-6 bg-white border rounded-lg hover:bg-gray-100"
+            className="flex flex-col max-w-custom-size h-80 p-6 border rounded-lg group hover:bg-banner03 hover:bg-cover"
           >
-            <h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-transparent">
               03
             </h4>
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-transparent">
               Web/App
             </h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-normal text-gray-700 dark:text-gray-400 group-hover:text-transparent">
               반응형 웹 또는 네이티브 앱, 하이브리드 앱을 개발해 드립니다.
               귀사의 다양한 비즈니스에 올드루키의 기술을 결합하여 성공적인 기술
               전문 파트너가 되어드립니다.
